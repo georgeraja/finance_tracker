dbf911f730bb2be70ca8d401d344b97b2cc6d19a
RajaGeorge <rajageorge971@gmail.com>
2025-01-07 23:29:06 +0000

    *Added Http Exception and Error handling in Routes

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -3,23 +3,24 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import accounts from "./accounts";
+import { HTTPException } from "hono/http-exception";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", clerkMiddleware(), (c) => {
-  const auth = getAuth(c);
-  if (!auth?.userId) {
-    return c.json({
-      error: "Unauthorized",
-    });
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return err.getResponse();
   }
-  return c.json({
-    message: "Hello world",
-    userId: auth?.userId,
-  });
+
+  return c.json({ err: "Internal Server Error" });
 });
+const routes = app.route("/accounts", accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
+
+//Generating RPC type, we also acheive end to end type safety with that
+export type AppType = typeof routes;
