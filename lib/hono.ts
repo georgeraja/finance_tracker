d8ad1125f0145453c534a33a17f5061272038059
RajaGeorge <rajageorge971@gmail.com>
2025-01-07 23:16:10 +0000

    *create RPC client

---
+++ lib/hono.ts
@@ -0,0 +1,6 @@
+//Create RPC client to use APPtype 
+
+import { hc } from "hono/client";
+import { AppType } from "@/app/api/[[...route]]/route";
+
+export const client = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL!);
