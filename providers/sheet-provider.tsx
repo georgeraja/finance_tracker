6929219c176a7da1ce81e96916b6138ea8087445
RajaGeorge <rajageorge971@gmail.com>
2025-03-10 21:56:34 +0000

    *added edit sheet in sheet provider

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -2,17 +2,18 @@
 import { useMountedState } from "react-use";
 
 import NewAccountSheet from "@/features/accounts/components/new-account-sheet";
+import EditAccountSheet from "@/features/accounts/components/edit-account-sheet";
 import React from "react";
 
 export const SheetProvider = () => {
-
-//same as useEffect code to fix hydration error
+  //same as useEffect code to fix hydration error
   const isMounted = useMountedState();
 
   if (!isMounted) return null;
   return (
     <>
       <NewAccountSheet />
+      <EditAccountSheet />
     </>
   );
 };
