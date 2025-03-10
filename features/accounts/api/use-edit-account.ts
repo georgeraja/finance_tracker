98fb86b445500634fbddf87806615c985aac648e
RajaGeorge <rajageorge971@gmail.com>
2025-03-10 21:55:20 +0000

    *created edit mutation

---
+++ features/accounts/api/use-edit-account.ts
@@ -0,0 +1,39 @@
+import { toast } from "sonner";
+import { InferRequestType, InferResponseType } from "hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { client } from "@/lib/hono";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.accounts)[":id"]["$patch"]
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.accounts)[":id"]["$patch"]
+>["json"];
+
+export const useEditAccount = (id?: string) => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async (json) => {
+      const response = await client.api.accounts[":id"]["$patch"]({
+        param: { id },
+        json,
+      });
+      return await response.json();
+    },
+
+    //fetching new accounts immidiately after creating the new one
+    onSuccess: () => {
+      toast.success("Account Updated");
+      queryClient.invalidateQueries({ queryKey: ["account", { id }] });
+      queryClient.invalidateQueries({ queryKey: ["accounts"] });
+
+      //Todo: Invalidate Summary and transaction
+    },
+    onError: () => {
+      toast.error("Failed to Edit Account");
+    },
+  });
+
+  return mutation;
+};
