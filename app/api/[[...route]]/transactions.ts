f04c6da453887eb358f0615f718c983987814e4e
RajaGeorge <rajageorge971@gmail.com>
2025-04-22 01:39:45 +0100

    *created API for Transaction

---
+++ app/api/[[...route]]/transactions.ts
@@ -0,0 +1,307 @@
+import { z } from "zod";
+import { Hono } from "hono";
+import { and, eq, gte, lte, inArray, desc, sql } from "drizzle-orm";
+import { subDays, parse } from "date-fns";
+import { db } from "@/db/drizzle";
+import {
+  transactions,
+  insertTransactionSchema,
+  categories,
+  accounts,
+} from "@/db/schema";
+import { createId } from "@paralleldrive/cuid2";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { zValidator } from "@hono/zod-validator";
+import { error } from "console";
+
+const app = new Hono()
+  .get(
+    "/",
+    zValidator(
+      "query",
+      z.object({
+        from: z.string().optional(),
+        to: z.string().optional(),
+        accountId: z.string().optional(),
+      })
+    ),
+    clerkMiddleware(),
+    async (c) => {
+      const auth = getAuth(c);
+      const { from, to, accountId } = c.req.valid("query");
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const defaultTo = new Date();
+      const defaultFrom = subDays(defaultTo, 30);
+
+      const startDate = from
+        ? parse(from, "yyyy-MM-dd", new Date())
+        : defaultFrom;
+
+      const endDate = to ? parse(to, "yyyy-MM-dd", new Date()) : defaultTo;
+      const data = await db
+        .select({
+          id: transactions.id,
+          date: transactions.date,
+          category: categories.name,
+          categoryId: transactions.categoryId,
+          payee: transactions.payee,
+          amount: transactions.amount,
+          notes: transactions.notes,
+          account: accounts.name,
+          accountId: transactions.accountId,
+        })
+        .from(transactions)
+        .innerJoin(accounts, eq(transactions.accountId, accounts.id))
+        .leftJoin(categories, eq(transactions.categoryId, categories.id))
+        .where(
+          and(
+            accountId ? eq(transactions.accountId, accountId) : undefined,
+            eq(accounts.userId, auth.userId),
+            gte(transactions.date, startDate),
+            lte(transactions.date, endDate)
+          )
+        )
+        .orderBy(desc(transactions.date));
+
+      return c.json({ data });
+    }
+  )
+  .get(
+    "/:id",
+    zValidator(
+      "param",
+      z.object({
+        id: z.string().optional(),
+      })
+    ),
+    clerkMiddleware(),
+    async (c) => {
+      const auth = getAuth(c);
+      const { id } = c.req.valid("param");
+      if (!id) {
+        return c.json({ error: "Missing Id" }, 400);
+      }
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const [data] = await db
+        .select({
+          id: transactions.id,
+          date: transactions.date,
+          categoryId: transactions.categoryId,
+          payee: transactions.payee,
+          amount: transactions.amount,
+          notes: transactions.notes,
+          accountId: transactions.accountId,
+        })
+        .from(transactions)
+        .innerJoin(accounts, eq(transactions.accountId, accounts.id))
+        .where(and(eq(transactions.id, id), eq(accounts.userId, auth.userId)));
+
+      if (!data) {
+        return c.json({ error: "not found" }, 404);
+      }
+      return c.json({ data });
+    }
+  )
+  .post(
+    "/",
+    clerkMiddleware(),
+    zValidator("json", insertTransactionSchema.omit({ id: true })),
+    async (c) => {
+      const auth = getAuth(c);
+      const values = c.req.valid("json");
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+      //we are using "returning" so insert could return something
+      const [data] = await db
+        .insert(transactions)
+        .values({
+          id: createId(),
+          ...values,
+        })
+        .returning();
+      return c.json({ data });
+    }
+  )
+  .post(
+    "/bulk-delete",
+    clerkMiddleware(),
+    zValidator(
+      "json",
+      z.object({
+        ids: z.array(z.string()),
+      })
+    ),
+    async (c) => {
+      const auth = getAuth(c);
+      const values = c.req.valid("json");
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const transactionsToDelete = db.$with("transaction_to_delete").as(
+        db
+          .select({
+            id: transactions.id,
+          })
+          .from(transactions)
+          .innerJoin(accounts, eq(transactions.accountId, accounts.id))
+          .where(
+            and(
+              inArray(transactions.id, values.ids),
+              eq(accounts.userId, auth.userId)
+            )
+          )
+      );
+      const data = await db
+        .with(transactionsToDelete)
+        .delete(transactions)
+        .where(
+          inArray(
+            transactions.id,
+            sql`(select id from ${transactionsToDelete})`
+          )
+        )
+        .returning({
+          id: transactions.id,
+        });
+
+      return c.json({ data });
+    }
+  )
+  .patch(
+    "/:id",
+    clerkMiddleware(),
+    zValidator(
+      "param",
+      z.object({
+        id: z.string().optional(),
+      })
+    ),
+    zValidator(
+      "json",
+      insertTransactionSchema.omit({
+        id: true,
+      })
+    ),
+    async (c) => {
+      const auth = getAuth(c);
+      const { id } = c.req.valid("param");
+      const values = c.req.valid("json");
+
+      if (!id) {
+        return c.json(
+          {
+            error: "Missing id",
+          },
+          400
+        );
+      }
+
+      if (!auth?.userId) {
+        return c.json(
+          {
+            error: "unauthorized",
+          },
+          401
+        );
+      }
+      const transactionsToUpdate = db.$with("transaction_to_update").as(
+        db
+          .select({
+            id: transactions.id,
+          })
+          .from(transactions)
+          .innerJoin(accounts, eq(transactions.accountId, accounts.id))
+          .where(and(eq(transactions.id, id), eq(accounts.userId, auth.userId)))
+      );
+
+      const [data] = await db
+        .with(transactionsToUpdate)
+        .update(transactions)
+        .set(values)
+        .where(
+          inArray(transactions.id, sql`(select id from transaction_to_update)`)
+        )
+        .returning();
+
+      if (!data) {
+        return c.json(
+          {
+            error: "Not found",
+          },
+          404
+        );
+      }
+      return c.json({ data });
+    }
+  )
+  .delete(
+    "/:id",
+    clerkMiddleware(),
+    zValidator(
+      "param",
+      z.object({
+        id: z.string().optional(),
+      })
+    ),
+
+    async (c) => {
+      const auth = getAuth(c);
+      const { id } = c.req.valid("param");
+
+      if (!id) {
+        return c.json(
+          {
+            error: "Missing id",
+          },
+          400
+        );
+      }
+
+      if (!auth?.userId) {
+        return c.json(
+          {
+            error: "unauthorized",
+          },
+          401
+        );
+      }
+
+      const transactionsToDelete = db.$with("transaction_to_delete").as(
+        db
+          .select({
+            id: transactions.id,
+          })
+          .from(transactions)
+          .innerJoin(accounts, eq(transactions.accountId, accounts.id))
+          .where(and(eq(transactions.id, id), eq(accounts.userId, auth.userId)))
+      );
+
+      const [data] = await db
+        .with(transactionsToDelete)
+        .delete(transactions)
+        .where(inArray(transactions.id, sql`(transactionsToDelete)`))
+        .returning({
+          id: transactions.id,
+        });
+
+      if (!data) {
+        return c.json(
+          {
+            error: "Not found",
+          },
+          404
+        );
+      }
+      return c.json({ data });
+    }
+  );
+
+export default app;
--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
-import { pgTable, text } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { createInsertSchema } from "drizzle-zod";
+import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
 export const accounts = pgTable("accounts", {
   id: text("id").primaryKey(),
@@ -7,6 +9,10 @@ export const accounts = pgTable("accounts", {
   name: text("name").notNull(),
   userId: text("user_id").notNull(),
 });
+
+export const accountsRelations = relations(accounts, ({ many }) => ({
+  transacations: many(transactions),
+}));
 
 export const insertAccountSchema = z.object({
   id: z.string().optional(),
@@ -22,6 +28,10 @@ export const categories = pgTable("categories", {
   userId: text("user_id").notNull(),
 });
 
+export const categoriesRelations = relations(categories, ({ many }) => ({
+  transacations: many(transactions),
+}));
+
 export const insertCategorySchema = z.object({
   id: z.string().optional(),
   plaidId: z.string().optional(),
@@ -29,4 +39,37 @@ export const insertCategorySchema = z.object({
   userId: z.string().min(1),
 });
 
-// export const insertAccountSchema = createInsertSchema(accounts);
+export const transactions = pgTable("transactions", {
+  id: text("id").primaryKey(),
+  amount: integer("amount").notNull(),
+  payee: text("payee").notNull(),
+  notes: text("notes"),
+  date: timestamp("date", { mode: "date" }).notNull(),
+  accountId: text("account_id").references(() => accounts.id, {
+    onDelete: "cascade",
+  }),
+  categoryId: text("category_id").references(() => categories.id, {
+    onDelete: "set null",
+  }),
+});
+
+export const transactionsRelations = relations(transactions, ({ one }) => ({
+  account: one(accounts, {
+    fields: [transactions.accountId],
+    references: [accounts.id],
+  }),
+  categories: one(categories, {
+    fields: [transactions.categoryId],
+    references: [categories.id],
+  }),
+}));
+
+export const insertTransactionSchema = z.object({
+  id: z.string().optional(),
+  amount: z.number().min(0),
+  payee: z.string().min(1),
+  notes: z.string().optional(),
+  date: z.coerce.date(),
+  accountId: z.string().min(1),
+  categoryId: z.string().optional(),
+});
